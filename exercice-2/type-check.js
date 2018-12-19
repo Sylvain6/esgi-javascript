bb76af33eaafacc3b591bcefe944005a47d45b5f
Sylvain Coutrot <s.coutrot.ext_azu@sodexo.com>
2018-12-19 15:14:34 +0100

    test

--- exercice-2/type-check.js
+++ exercice-2/type-check.js
@@ -1,8 +1,17 @@
-function type_check_v1(value, type){
+const type_check_v1 = (value, type) => {
     if(typeof(value) !== type){
         return false;
     }
     return true;
 };
 
-console.log(type_check_v1(1, "number"));
+function type_check_v2(val,args) {
+    if(typeof val == "object"){
+        if(compareObj(val,args.value)){
+            return type_check_v1(val,args.type);
+        }
+    }else if(val == args.value){
+        return type_check_v1(val,args.type);
+    }
+    return false;
+}
